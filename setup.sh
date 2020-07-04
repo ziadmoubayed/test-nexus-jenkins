05f1bb7b5729ddf28d02789f1029e80db282cb09
Ziadm <ziadmoubayed@gmail.com>
2020-07-04 17:56:13 +0200

    pip work

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 !#/bin/bash
 echo "Hello World"
-python -m venv venv
+pip install --upgrade pip
+pip install virtualenv 
+virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
 pip show mlflow
