0d68786ea3fbf8f650b34c95b1923dfb62c4ed31
Ziadm <ziadmoubayed@gmail.com>
2020-07-04 18:22:30 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-!#/bin/bash
+#!/bin/sh
 echo "Hello World"
-apt-get update && apt-get upgrade -y && apt-get clean
-apt-get install -y curl python3.7 python3.7-dev python3.7-distutils
-update-alternatives --install /usr/bin/python python /usr/bin/python3.7 1
-update-alternatives --set python /usr/bin/python3.7
+sudo apt-get update
+sudo apt-get install -y curl python3.7 python3.7-dev python3.7-distutils
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 1
+sudo update-alternatives --set python /usr/bin/python3.7
  curl -s https://bootstrap.pypa.io/get-pip.py -o get-pip.py && \
     python get-pip.py --force-reinstall && \
     rm get-pip.py
