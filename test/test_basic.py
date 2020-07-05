ca8172caf3ce74cea4330d83b44addcbb5b6a070
Ziadm <ziadmoubayed@gmail.com>
2020-07-05 13:22:50 +0200

    fix

--- test/test_basic.py
+++ test/test_basic.py
@@ -4,7 +4,7 @@ import pytest
 def f():
     raise SystemExit(1)
 
-
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_mytest():
     with pytest.raises(SystemExit):
         f()
